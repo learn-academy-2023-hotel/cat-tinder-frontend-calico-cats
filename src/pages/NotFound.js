952a6ae4372691b424dfe2c02f52de4eccfa9650
zeniavarela <zvarela8@gmail.com>
2023-12-08 11:24:23 -0800

    structure, no styling

---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const NotFound = () => {
+  return (
+    <>
+      <div className="not-found">NotFound</div>
+    </>
+  )
+}
+export default NotFound
---
+++ src/pages/PetEdit.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const PetEdit = () => {
+  return (
+    <>
+      <div>PetEdit</div>
+    </>
+  )
+}
+export default PetEdit
