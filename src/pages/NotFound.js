d8182fd9473d41e374fb3a8f7390da0a650b2dae
zeniavarela <zvarela8@gmail.com>
2023-12-14 10:24:18 -0800

    Not found page styling

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,13 +1,17 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 
 const NotFound = () => {
   return (
     <>
       <div className="not-found">
-        <p>
-        404
-        </p>
-      Page Not Found</div>
+        <p>404</p>
+        Page Not Found
+        <div> Return home?</div>
+      </div>
+      <NavLink to={`/`} className="nav-link">
+        <button className="button-show">Go Home!</button>
+      </NavLink>
     </>
   )
 }
